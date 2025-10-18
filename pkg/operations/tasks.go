ed4ad3b86ed1177d11fdbba4bb144e7fdeb65fe4
riverfog7 <cjw9770@gmail.com>
2025-10-19 02:20:38 +0900

    Add task runner

--- pkg/operations/tasks.go
+++ pkg/operations/tasks.go
@@ -1,6 +1,7 @@
 package operations
 
 import "SophonClientv2/internal/models"
+import "SophonClientv2/internal/logging"
 
 func PerformInstall(request models.InstallRequest) models.TaskResponse {
 	// TODO: Implement install
@@ -15,4 +16,24 @@ func PerformRepair(request models.RepairRequest) models.TaskResponse {
 func PerformUpdate(request models.UpdateRequest) models.TaskResponse {
 	// TODO: Implement update
 	return models.TaskResponse{}
+}
+
+func RunTask(taskType string, request interface{}) models.TaskResponse {
+	switch taskType {
+	case "install":
+		if req, ok := request.(models.InstallRequest); ok {
+			return PerformInstall(req)
+		}
+	case "repair":
+		if req, ok := request.(models.RepairRequest); ok {
+			return PerformRepair(req)
+		}
+	case "update":
+		if req, ok := request.(models.UpdateRequest); ok {
+			return PerformUpdate(req)
+		}
+	default:
+		logging.GlobalLogger.Fatal("Unknown task type: " + taskType)
+	}
+	return models.TaskResponse{} // Should not reach here
 }
