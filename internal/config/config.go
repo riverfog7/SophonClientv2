5f6b785a1fe224d136412c1ad9583228df936f0a
riverfog7 <cjw9770@gmail.com>
2025-10-19 14:41:46 +0900

    Add config for threadcount for other actions (Decomp, HashCheck)

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,10 +4,14 @@ type SophonClientConfig struct {
 	MaxManifestDownloadRetries int
 	MaxChunkDownloadRetries    int
 	CocurrentDownloads         int
+	CocurrentDecompressions    int
+	CocurrentHashchecks        int
 }
 
 var Config SophonClientConfig = SophonClientConfig{
 	MaxManifestDownloadRetries: 5,
 	MaxChunkDownloadRetries:    5,
 	CocurrentDownloads:         16,
+	CocurrentDecompressions:    4,
+	CocurrentHashchecks:        8,
 }
